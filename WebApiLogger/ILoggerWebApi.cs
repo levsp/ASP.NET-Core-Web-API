68481a5f2ae5163ee87b66c4cf5ae80ea90d1015
levsp <40392164+levsp@users.noreply.github.com>
2020-05-22 10:34:51 +0300

    Add files via upload

---
+++ WebApiLogger/ILoggerWebApi.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WebApiLogger
+{
+    public interface ILoggerWebApi
+    {
+        void LogInfo(string message);
+        void LogError(string message);
+    }
+}
---
+++ WebApiLogger/LoggerWebApi.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NLog;
+
+namespace WebApiLogger
+{
+    public class LoggerWebApi : ILoggerWebApi
+    {
+        private static ILogger logger = LogManager.GetCurrentClassLogger();
+
+        public void LogError(string message)
+        {
+            logger.Error(message);
+        }
+
+        public void LogInfo(string message)
+        {
+            logger.Info(message);
+        }
+    }
+}
