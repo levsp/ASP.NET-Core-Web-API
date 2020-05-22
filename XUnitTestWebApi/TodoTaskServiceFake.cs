6dbce9532361e7fa4edbd417becea9dd34019550
levsp <40392164+levsp@users.noreply.github.com>
2020-05-22 10:54:37 +0300

    Add files via upload

---
+++ XUnitTestWebApi/TodoTaskServiceFake.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using WebApi.Entities;
+using WebApi.Services.Interfaces;
+
+namespace XUnitTestWebApi
+{
+    public class TodoTaskServiceFake : ITodoTaskService
+    {
+        private readonly List<TodoTask> _todoTask;
+
+        public TodoTaskServiceFake()
+        {
+            _todoTask = new List<TodoTask>()
+            {
+                new TodoTask()
+                {
+                    TodoTaskId = 1,
+                    Name = "Write",
+                    DueDate = DateTime.Now.AddDays(3),
+                    City = "LA",
+                    AssignedTo = "qq@gmail.com",
+                    PriorityId = 1,
+                    StatusId = 1
+                },
+                new TodoTask()
+                {
+                    TodoTaskId = 2,
+                    Name = "Read",
+                    DueDate = DateTime.Now.AddDays(3),
+                    City = "LA",
+                    AssignedTo = "qq@gmail.com",
+                    PriorityId = 1,
+                    StatusId = 1
+                },
+                new TodoTask()
+                {
+                    TodoTaskId = 3,
+                    Name = "Run",
+                    DueDate = DateTime.Now.AddDays(3),
+                    City = "LA",
+                    AssignedTo = "qq@gmail.com",
+                    PriorityId = 1,
+                    StatusId = 1
+                }
+            };
+        }
+
+        public void Add(TodoTask entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(TodoTask entity)
+        {
+            var existing = _todoTask.First(a => a.TodoTaskId == entity.TodoTaskId);
+            _todoTask.Remove(existing);
+        }
+
+        public TodoTask Get(int id)
+        {
+            return _todoTask.Where(a => a.TodoTaskId == id)
+            .FirstOrDefault();
+        }
+
+        public IEnumerable<TodoTask> GetAll()
+        {
+            return _todoTask;
+        }
+
+        public IEnumerable<TodoTask> GetByFilters(IEnumerable<Filter> filters)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(TodoTask dbEntity, TodoTask entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ XUnitTestWebApi/TodoTasksControllerTest.cs
@@ -0,0 +1,65 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using WebApi.Controllers;
+using WebApi.Services.Interfaces;
+using WebApiLogger;
+using Xunit;
+
+namespace XUnitTestWebApi
+{
+    public class TodoTasksControllerTest
+    {
+        TodoTasksController _controller;
+        ITodoTaskService _service;
+        IMapper _mapper;
+        ILoggerWebApi _logger;
+
+        public TodoTasksControllerTest()
+        {
+            _service = new TodoTaskServiceFake();
+            _controller = new TodoTasksController(_service, _mapper, _logger);
+        }
+
+        [Fact]
+        public void Remove_NotExistingIdPassed_ReturnsNotFoundResponse()
+        {
+            // Arrange
+            var notExistingId = -1;
+
+            // Act
+            var badResponse = _controller.Delete(notExistingId);
+
+            // Assert
+            Assert.IsType<NotFoundObjectResult>(badResponse);
+        }
+
+        [Fact]
+        public void Remove_ExistingIdPassed_ReturnsOkResult()
+        {
+            // Arrange
+            var existingGuid = 1;
+
+            // Act
+            var okResponse = _controller.Delete(existingGuid);
+
+            // Assert
+            Assert.IsType<NoContentResult>(okResponse);
+        }
+        [Fact]
+        public void Remove_ExistingIdPassed_RemovesOneItem()
+        {
+            // Arrange
+            var existingId = 2;
+
+            // Act
+            var okResponse = _controller.Delete(existingId);
+
+            // Assert
+            Assert.Equal(2, _service.GetAll().Count());
+        }
+    }
+}
